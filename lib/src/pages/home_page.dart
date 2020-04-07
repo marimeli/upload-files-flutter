42b2283e4e1630822c90d6fe29d2988523cb9430
marimeli <marimel.casas@gmail.com>
2020-04-06 19:08:48 -0500

    using dio to upload a file

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -45,7 +45,7 @@ class _HomePageState extends State<HomePage> {
               padding: EdgeInsets.only(top: 50.0, bottom: 20.0),
               child: RaisedButton(
                 onPressed: () => _openFileExplorer(),
-                child: Text("Search file"),
+                child: Text("Open file picker"),
               ),
             ),
           ],
